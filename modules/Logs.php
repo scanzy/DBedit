a29fbf3a7a35df0be9176a6abb508608b2d7523b
scanzy <matteoscanzano@yahoo.it>
2017-02-13 11:21:17 +0100

    Add newline in logs

--- modules/Logs.php
+++ modules/Logs.php
@@ -14,13 +14,13 @@ class Logs
 
     //logs error
     public static function Error($msg, $isServerError = TRUE)
-     { self::AppendWithTime(self::$ERRORS_FILE, Auth::username(), (($isServerError) ? "SERVER" : "CLIENT")." ERROR: ".$msg); }
+     { self::AppendWithTime(self::$ERRORS_FILE, Auth::username(), (($isServerError) ? "SERVER" : "CLIENT")." ERROR: ".$msg."\n"); }
 
     //logs access
     public static function Login($user, $ok = TRUE) 
     { self::AppendWithTime(self::$ACCESSES_FILE, $user, "LOGIN ".(($ok) ? "OK" : "FAIL")); }
 
     //logs generic string 
-    static function AppendWithTime($file, $user, $msg) { self::Append($file, date('Y-m-d H:i:s')." user: '$user' \t\t message: $msg"); }
+    static function AppendWithTime($file, $user, $msg) { self::Append($file, date('Y-m-d H:i:s')." user: '$user' \t\t message: $msg\n"); }
     static function Append($file, $newline) { @file_put_contents($file, $newline.PHP_EOL, FILE_APPEND | LOCK_EX); }
 }
